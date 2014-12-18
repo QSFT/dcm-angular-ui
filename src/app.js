26938eb43e6213281ec38d1f2020dcf806f7817d
Nick Shearer <nick.shearer@software.dell.com>
2014-12-17 20:02:47 -0600

    doc update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,1 @@
-angular.module('dcm-ui', ['dcm-ui.resizable-cols', 'dcm-ui.multiple-input', 'dcm-ui.grid', 'dcm-ui.select2', 'dcm-ui.slider']);
+angular.module('dcm-ui', ['dcm-ui.resizable-cols', 'dcm-ui.multiple-input', 'dcm-ui.password', 'dcm-ui.grid', 'dcm-ui.select2', 'dcm-ui.slider']);
